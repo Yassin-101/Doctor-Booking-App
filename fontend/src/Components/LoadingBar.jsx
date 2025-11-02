0b993abed554286eee4f729ab5d4e93491b1fd5a
Mirza Yassin <mirza.yassin9@gmail.com>
2025-11-02 16:18:56 +0400

    added loading effect in app.jsx

--- fontend/src/Components/LoadingBar.jsx
+++ fontend/src/Components/LoadingBar.jsx
@@ -1,16 +1,14 @@
 // src/components/LoadingBar.jsx
 import React from 'react'
 
-const LoadingBar = ({ active }) => {
-  return (
-    // container (fixed at top)
-    <div className="fixed top-0 left-0 w-full h-1 z-50 pointer-events-none">
-      {/* inner bar animates width from 0 -> 100% (tailwind classes used) */}
-      <div
-        className={`h-full transition-all duration-1000 ease-out ${active ? 'w-full bg-blue-600' : 'w-0 bg-transparent'}`}
-      />
-    </div>
-  )
-}
+const LoadingBar = ({ active }) => (
+  <div className="fixed top-0 left-0 w-full h-1 z-50">
+    <div
+      className={`h-full bg-blue-600 transition-all duration-1000 ease-out ${
+        active ? 'w-full' : 'w-0'
+      }`}
+    />
+  </div>
+)
 
 export default LoadingBar
