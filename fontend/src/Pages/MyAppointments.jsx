d194ea985150fa5bd2d91883a1a4067827d4db6a
Mirza Yassin <mirza.yassin9@gmail.com>
2025-11-03 17:04:55 +0400

    added myappointments page information

--- fontend/src/Pages/MyAppointments.jsx
+++ fontend/src/Pages/MyAppointments.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import {AppContext} from '../Context/AppContext'
 
 const MyAppointments = () => {
+
+  const {doctors} = useContext(AppContext)
   return (
-    <div>
-      
+    <div className='mx-4 sm:mx-[10%]'>
+      <p>My Appoinments</p>
+
+      <div>
+        {doctors.slice(0,2).map((item,index)=>(
+          <div key={index}>
+              <div>
+                <img src={item.image} alt="" />
+              </div>
+
+              <div>
+                <p>{item.name}</p>
+                <p>{item.speciality}</p>
+                <p>Adress:</p>
+                <p>{item.address.line1}</p>
+                <p>{item.address.line2}</p>
+                <p><span>Date & Time:</span>25, July, 2024 | 8:30 PM</p>
+              </div>
+                    {/* to make this structure responsive */}
+              <div></div> 
+
+              <div>
+                <button>Pay Online</button>
+                <button>Cancel appointment</button>
+              </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
